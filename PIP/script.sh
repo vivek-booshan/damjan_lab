24511bc6c6de466de3461eb3a969b4c6ff186e1f
Vivek Booshan <vivek.booshan@gmail.com>
2023-06-20 15:59:46 -0400

    random changes

--- PIP/script.sh
+++ PIP/script.sh
@@ -6,26 +6,30 @@
 
 ### LOAD MEMBRANE ### 
 
-# vmd -dispdev text << EOF #cat << EOF > velocity.vmdin
+vmd -dispdev text << EOF #cat << EOF > velocity.vmdin
 
-# source load_membrane.tcl
+set cwd "/home/vivek/repos/damjan_lab/PIP"
+for {set i 1} {\$i <= 10} {incr i} {
+  mol addfile "/home/vivek/repos/damjan_lab/PIP/nc/eq.\$i.nc"
 
-# set numframes [molinfo top get numframes]
-# set sum 0
-# for {set i 0} {\$i <= \$numframes} {incr i} {
-#     set sum [expr {\$sum + 1}]
-# }
-# puts \$sum
-# exit
-# EOF
+
+  set numframes [molinfo top get numframes]
+  set sum 0
+  for {set i 0} {\$i <= \$numframes} {incr i} {
+      set sum [expr {\$sum + 1}]
+  }
+  puts \$sum
+}
+exit
+EOF
 
 ## Prints 13 ##
 
 ###############################################
 
-vmd -dispdev text -e load_membrane.tcl << EOF
-source velocities.tcl
-exit
-EOF
+# vmd -dispdev text -e load_membrane.tcl << EOF
+# source velocities.tcl
+# exit
+# EOF
 
 ##################################################
